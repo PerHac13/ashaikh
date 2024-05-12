71c8033c6b469d58892c52be979b1125b078b5cf
perhac <abdullah13112004@gmail.com>
2024-05-12 23:50:09 +0530

    Create: Experience section

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,9 +1,70 @@
 "use client"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card";
+  import { Badge } from "@/components/ui/badge";
+  import { MoveRight } from "lucide-react";
+
+  import { jobPositions } from "../constants/jobPositions";
 
 export default function Experience() {
     return (
-        <section id="experience">
-            <h2>Experience</h2>
-        </section>
+        <section id="experience" className="scroll-mt-16 lg:mt-16">
+      <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-background/0 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+        <h2 className="text-sm font-bold uppercase tracking-widest lg:sr-only">
+          Experience
+        </h2>
+      </div>
+      <>
+        {jobPositions.map((job, index) => (
+          <Card
+            key={index}
+            className="lg:p-6 mb-8 flex flex-col lg:flex-row w-full min-h-fit gap-0 lg:gap-5 border-transparent hover:border dark:lg:hover:border-t-blue-900 dark:lg:hover:bg-slate-800/50 lg:hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:hover:drop-shadow-lg lg:hover:bg-slate-100/50 lg:hover:border-t-blue-200"
+          >
+            <CardHeader className="h-full w-full p-0">
+              <CardTitle className="text-base text-slate-400 whitespace-nowrap">
+                {job.timeline}
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col p-0">
+              <p className="text-primary font-bold">
+                {job.currentPosition} • {job.place}
+              </p>
+              {job.previousPositions.map((position, index) => (
+                <p key={index} className="text-slate-400 text-sm font-bold">
+                  {position}
+                </p>
+              ))}
+              <CardDescription className="py-3 text-muted-foreground">
+                {job.description.map((desc, index) => (
+                    <p key={index}>{desc}</p>
+                    ))}
+              </CardDescription>
+              <CardFooter className="p-0 flex flex-wrap gap-2">
+                {job.skills.map((skill, index) => (
+                  <Badge key={index}>{skill}</Badge>
+                ))}
+              </CardFooter>
+            </CardContent>
+          </Card>
+        ))}
+      </>
+      <div className="lg:px-12 mt-12">
+        <a
+          className="inline-flex items-center font-medium leading-tight text-foreground group"
+          href="/resume/resume.pdf"
+        >
+          <span className="border-b border-transparent pb-px transition hover:border-primary motion-reduce:transition-none">
+            View Full Resume
+          </span>
+          <MoveRight className="ml-1 inline-block h-5 w-5 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2 motion-reduce:transition-none" />
+        </a>
+      </div>
+    </section>
     );
 }
