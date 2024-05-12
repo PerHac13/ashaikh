e8efc54e989a0284f96f1c4eea8d6098a6bfb670
perhac <abdullah13112004@gmail.com>
2024-05-12 23:39:16 +0530

    Add: constant data

---
+++ constants/jobPositions.ts
@@ -0,0 +1,40 @@
+export const jobPositions = [
+    {
+        "timeline": "Mar 2024 - Present",
+        "currentPosition": "Frontend Developer - Internship",
+        "place": "BotGauge",
+        "previousPositions": [""],
+        "description":[
+          "Worked on the development of the company's website, to provide seamless and efficient user interaction with automation suite.",
+          "Integrated over 8 APIs, developed 4+ pages, and optimized workflow and speed by 30% through code refactoring."],
+        "skills": [
+          "React",
+          "CSS",
+          "Redux",
+          "UI/UX",
+          "Automation",
+          "Scripting"
+        ]
+      },
+      {
+        "timeline": "Feb 2024 - Present",
+        "currentPosition": "Open Source Lead",
+        "place": "DevC (IIIT Bhagalpur)",
+        "previousPositions": ["Core Team Member"],
+        "description":[
+            "Currently developing the backend for the institute's Gymkhana Board website as a Backend Developer.",
+            "Successfully organized OPCODE one-month open spring fest, establishing over 35 open-source repositories, generating over 250 issues, and receiving more than 500 pull requests.",
+            "Conducted 10+ introductory sessions on various subjects to facilitate learning and skill development."
+
+        ],
+        "skills": [
+            "React",
+            "JavaScript",
+            "Node.js",
+            "express.js",
+            "Management",
+            "Problem-Solving",
+            "System documentation"   
+        ]
+      }
+]
