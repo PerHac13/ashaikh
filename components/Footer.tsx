06d5654eed092da29749c6eaee8f61a30e9e14cc
perhac <abdullah13112004@gmail.com>
2024-05-13 21:04:06 +0530

    Update: Footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,37 @@
-"use client"
+"use client";
 
 export default function Footer() {
-    return (
-        <section id="footer">
-            <h2>Footer</h2>
-        </section>
-    );
+  return (
+    <section>
+      <div className="flex flex-col gap-4 lg:px-6 mt-16">
+        <p className="text-sm text-start text-muted-foreground">
+          Inspired by{" "}
+          <a className="text-foreground" href="https://brittanychiang.com">
+            Brittany Chiang&#39;s website
+          </a>{" "}
+          . Coded in{" "}
+          <a className="text-foreground" href="https://code.visualstudio.com/">
+            Visual Studio Code.
+          </a>{" "}
+          Built with{" "}
+          <a className="text-foreground" href="https://nextjs.org/">
+            Next.js
+          </a>
+          ,{" "}
+          <a className="text-foreground" href="https://tailwindcss.com/">
+            Tailwind CSS
+          </a>{" "}
+          and{" "}
+          <a className="text-foreground" href="https://ui.shadcn.com/">
+            Shadcn/ui
+          </a>
+          , deployed with{" "}
+          <a className="text-foreground" href="https://vercel.com/">
+            Vercel
+          </a>
+          .
+        </p>
+      </div>
+    </section>
+  );
 }
