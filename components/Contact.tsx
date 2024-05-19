023e9100b2e67c03d4f1cd2c9361c93351f698eb
perhac <abdullah13112004@gmail.com>
2024-05-19 16:18:03 +0530

    update: contact info

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -23,7 +23,7 @@ export default function Contact() {
       </div>
       <div className="flex flex-row justify-center items-center gap-4 lg:px-6 mb-4">
         <a
-          href="mailto:abdullah13112004@gmail.com"
+          href="mailto:shaikh.2201027cs@iiitbh.ac.in"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:cursor-pointer w-full"
