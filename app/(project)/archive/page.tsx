d020fa05cd754f006c321c5ff936a29b99b76e40
perhac <abdullah13112004@gmail.com>
2024-05-19 16:15:50 +0530

    Add: archive page( WIP)

--- app/(project)/archive/page.tsx
+++ app/(project)/archive/page.tsx
@@ -13,7 +13,8 @@ export default function Home() {
  
 
   return (
-    <>
+    
+      <>
       <Head>
         <style jsx global>{`
           body {
@@ -22,8 +23,10 @@ export default function Home() {
         `}</style>
       </Head>
       <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
-        <div className="lg:flex lg:justify-between lg:gap-4">
-          Archive is work in progress
+        <div className="lg:flex lg:justify-center lg:gap-4">
+          <main className="flex flex-col pt-6 lg:pt-24 lg:w-1/2 lg:py-24 gap-8 text-center text-4xl">
+            Work In Progress
+          </main>
         </div>
       </div>
     </>
