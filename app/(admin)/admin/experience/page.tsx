262883bbd7d7ae20c100ba82fe494cbe671edc58
PerHac13 <abdullah13112004@gmail.com>
2025-03-19 11:52:51 +0530

    feat (ui): admin experence page

--- app/(admin)/admin/experience/page.tsx
+++ app/(admin)/admin/experience/page.tsx
@@ -1,10 +1,20 @@
-"use client";
+import ExperienceCreateButton from "../_components/experience/ExperienceCreateButton";
+import ExperienceList from "../_components/experience/ExperienceList";
 
 const ExperiencePage = () => {
   return (
-    <div>
-      <h1>Experience Page</h1>
+    <>
+      <div className="container mx-auto px-4 py-8">
+        <div className="flex justify-between items-center">
+          <h1 className="text-2xl font-bold">Experience</h1>
+          <ExperienceCreateButton />
+        </div>
+      </div>
+    
+    <div className="container mx-auto px-4 py-8">
+      <ExperienceList />
     </div>
+    </>
   );
 };
 
