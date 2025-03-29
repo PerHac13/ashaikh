9765ef1b11408915341919f8b13d95566392598a
PerHac13 <abdullah13112004@gmail.com>
2025-03-29 22:41:35 +0530

    chore: new env

--- lib/env.ts
+++ lib/env.ts
@@ -1,9 +1,11 @@
 import zod from "zod";
 
 const envSchema = zod.object({
-    // PORT: zod.number().default(3000), 
-    NODE_ENV: zod.string().default('development'),
-    MONGODB_URI: zod.string().nonempty(),
+  // PORT: zod.number().default(3000),
+  NODE_ENV: zod.string().default("development"),
+  MONGODB_URI: zod.string(),
+  NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: zod.string(),
+  NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET: zod.string(),
 });
 
 export const env = envSchema.parse(process.env);
