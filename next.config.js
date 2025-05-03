69d04fa66cf23bfac01cba89e9f65f2c3ab9b393
PerHac13 <abdullah13112004@gmail.com>
2025-05-03 21:29:52 +0530

    feat!: update config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["socialify.git.ci"],
+    domains: [
+      "socialify.git.ci",
+      "raw.githubusercontent.com",
+      "res.cloudinary.com",
+    ],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
 
